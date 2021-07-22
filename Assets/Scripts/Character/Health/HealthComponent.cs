22335789e5b20de0e3b16f6bb696b75632c7b0c0
LiliaZheltiakova <66412276+LiliaZheltiakova@users.noreply.github.com>
2021-07-22 15:32:33 +0800

    test

---
+++ Assets/Scripts/Character/Health/HealthComponent.cs
@@ -0,0 +1,48 @@
+﻿using Assets.Scripts.Character.Attack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Character.Health
+{
+    public class HealthComponent : MonoBehaviour
+    {
+        [SerializeField] private int health;
+        public int Health { get => health; }
+
+        [SerializeField] private bool isDead;
+        public bool IsDead { get => isDead; }
+
+        public Action<int> OnHealthChanged;
+        public Action OnDead;
+        public string soundName;
+
+        private DamageEffect damageEffect;
+        private PlaySound playSound;
+
+        void Start()
+        {
+            damageEffect = GetComponent<DamageEffect>();
+            playSound = GetComponentInChildren<PlaySound>();
+        }
+        
+        public void ApplyDamage(AttackComponent attackComponent)
+        {
+            health -= attackComponent.Damage;
+            if (damageEffect) damageEffect.ShowDamageEffect();
+            if (playSound) playSound.Play(soundName);
+
+            if(health <=0)
+            {
+                isDead = true;
+                health = 0;
+                OnDead?.Invoke();
+            }
+
+            OnHealthChanged?.Invoke(health);
+        }
+    }
+}
